09de08b4ef51fef35c1d4c5e57f6bb268351b390
Angelium-Eddra <angelium.eddra1972@gmail.com>
2024-05-10 23:44:59 -0300

    refinando a I.A.

--- metadata.py
+++ metadata.py
@@ -1,3 +1,10 @@
 data = [
-    
+    "Matéria Matemática",
+  "Conteúdo Teorema de Pitágoras",
+  "questões 1- Qual é as relações presentes entre os lados de um triângulo retângulo?\n\n2- Descreva o nome dado aos lados do mesmo.\n\n3- Quais são as aplicações do teorema?",
+  "output 2: 1- A relação se dá pela fórmula a² = b² + c², sendo a, b, e c os lados do triângulo.\n\n2- O maior lado é chamado de hipotenusa, e os lados que formam o ângulo reto são chamados de catetos.\n\n3- Podemos usar o teorema para medir distâncias indiretamente, por exemplo.",
+  "Matéria Geografia",
+  "Conteúdo Placas tectônicas",
+  "questões 1- Quais são os tipos de movimentos das placas tectônicas?\n\n2- Diga dois tipos de relevos formados pelas placas.",
+  "output 2: 1- Os movimentos podem ser tanto de afastamento, quanto de aproximação.\n\n2- Os relevos podem ser cadeias de montanhas e planaltos."
 ]
